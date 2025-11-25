da1aa7393bf25775d67709770e261b79df175c09
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-11-25 16:43:46 +0530

    airqualityApplication

---
+++ src/main/java/com/example/aqicn/AqicnApplication.java
@@ -0,0 +1,13 @@
+package com.example.aqicn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqicnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqicnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aqicn/AqicnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aqicn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqicnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
